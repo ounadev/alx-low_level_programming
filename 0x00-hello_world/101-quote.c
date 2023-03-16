4276e38ff2a0abc0e4e9553fa66aa9a4964ea316
ounadev <ounaissa19@gmail.com>
2023-03-16 10:46:42 -0700

    first

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,20 @@
 #include <unistd.h>
+
 /**
  * main - Entry point
  *
- * Return: Always 1 (Error)
+ * Description: Prints a message to the standard error
+ * without using printf or puts.
+ *
+ * Return: 1 (Error code)
  */
+
 int main(void)
 {
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return (1);
+char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+write(2, msg, sizeof(msg) - 1);
+
+return (1);
 }
+
