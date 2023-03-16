5c7c8c093830badd67f9606e88e73201c3bb37e4
ounadev <ounaissa19@gmail.com>
2023-03-16 10:13:04 -0700

    first

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,10 @@
 #include <stdio.h>
+/* This is a simple program that prints a message */
 
-int main(void){puts("\"Programming is like building a multilingual puzzle");return(0);}
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    puts("");
+    return 0;
+}
+
