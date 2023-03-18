b8acece26b0f7d8f924466ae26fba725b340043b
ounadev <ounaissa19@gmail.com>
2023-03-18 06:10:44 -0700

    first

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 97; i <= 122; i++)
+	putchar(i);
+for (i = 65; i <= 90; i++)
+	putchar(i);
+putchar('\n');
+
+return (0);
+}
