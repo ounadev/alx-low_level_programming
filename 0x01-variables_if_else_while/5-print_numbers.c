fec9015d5447162720e6519b6497dcd33cbf105f
ounadev <ounaissa19@gmail.com>
2023-03-18 06:29:46 -0700

    first

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+
+i = 0;
+	while (i < 10)
+{
+putchar(i + '0');
+i++;
+}
+putchar('\n');
+
+return (0);
+}
