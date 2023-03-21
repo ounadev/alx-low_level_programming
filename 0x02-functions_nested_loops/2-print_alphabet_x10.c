d0605e3a83e41e9e215b0051ea1efcf252feef95
ounadev <ounaissa19@gmail.com>
2023-03-21 15:53:38 -0700

    first

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
@@ -15,3 +16,4 @@ void print_alphabet_x10(void)
         putchar('\n');
     }
 }
+
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -4,5 +4,6 @@
 int main(void)
 {
     print_alphabet();
+    print_alphabet_x10();
     return (0);
 }
