cf4093c17a1eaa6ca4d28b8faac7aa94ee703f42
ounadev <ounaissa19@gmail.com>
2023-03-23 11:14:27 -0700

    first

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,7 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void more_numbers(void);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
