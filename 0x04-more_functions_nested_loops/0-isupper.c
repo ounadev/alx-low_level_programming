4f40b9286a1979db5271c2391713dec0442aba98
ounadev <ounaissa19@gmail.com>
2023-03-23 10:11:56 -0700

    first

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,16 @@
 #include <stdio.h>
 /**
  * main - check the code.
- *
  * Return: Always 0.
  */
 int _isupper(int c)
 {
-    if (c >= 'A' && c <= 'Z') {
-        return 1;
-    } else {
-        return 0;
-    }
+if (c >= 'A' && c <= 'Z')
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
 }
