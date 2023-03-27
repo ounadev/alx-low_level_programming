cb163dc79dc7e3f86d51a834cccefc1b96d70e01
ounadev <ounaissa19@gmail.com>
2023-03-27 14:15:02 -0700

    first

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,14 +6,17 @@
  */
 void print_rev(char *s)
 {
-int len = 0;
-while (s[len] != '\0')
-{
-	len++;
-}
-for (int i = len - 1; i >= 0; i--)
-{
-	_putchar(s[i]);
-}
-_putchar('\n');
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	for (int i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
 }
