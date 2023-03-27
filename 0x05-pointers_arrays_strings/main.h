83fc6d9cb280d0d289fbeaeaee57370f83fbb3d7
ounadev <ounaissa19@gmail.com>
2023-03-27 14:09:12 -0700

    first

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,3 +2,9 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+#endif /* MAIN_H */
