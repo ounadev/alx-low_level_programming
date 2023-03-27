1ca59b514f16591d48ed2af6c5c3c4503fdcafdc
ounadev <ounaissa19@gmail.com>
2023-03-27 14:55:49 -0700

    first

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * rev_string - Reverses a string in place.
  *
@@ -7,18 +6,14 @@
  */
 void rev_string(char *s)
 {
-    int i, j;
-    char temp;
-
-    /* Find the length of the string */
-    for (i = 0; s[i] != '\0'; i++)
-        ;
-
-    /* Reverse the string */
-    for (i--, j = 0; j < i; j++, i--)
-    {
-        temp = s[j];
-        s[j] = s[i];
-        s[i] = temp;
-    }
+int i, j;
+char temp;
+for (i = 0; s[i] != '\0'; i++)
+	;
+for (i--, j = 0; j < i; j++, i--)
+{
+	temp = s[j];
+	s[j] = s[i];
+	s[i] = temp;
+}
 }
