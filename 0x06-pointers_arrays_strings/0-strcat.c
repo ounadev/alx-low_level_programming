dd3f597437f4af3d7b807792b52c2511d71f0973
ounadev <ounaissa19@gmail.com>
2023-03-30 02:46:51 -0700

    first

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strcat - concatenates two strings
  * @dest: the destination string
@@ -9,15 +8,11 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    char *dest_end = dest;
-
-    while (*dest_end)
-        dest_end++;
-
-    while (*src)
-        *dest_end++ = *src++;
-
-    *dest_end = '\0';
-
-    return dest;
+char *dest_end = dest;
+while (*dest_end)
+	dest_end++;
+while (*src)
+	*dest_end++ = *src++;
+*dest_end = '\0';
+return (dest);
 }
