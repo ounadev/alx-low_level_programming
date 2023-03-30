fe9133d001b129f453a0d8d306777efcbcb93dda
ounadev <ounaissa19@gmail.com>
2023-03-30 02:59:45 -0700

    first

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strcmp - compare two strings
  * @s1: the first string
@@ -10,26 +9,24 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-    int i;
-
-    for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
-    {
-        if (s1[i] != s2[i])
-        {
-            return (s1[i] - s2[i]);
-        }
-    }
-
-    if (s1[i] == s2[i])
-    {
-        return (0);
-    }
-    else if (s1[i] == '\0')
-    {
-        return (-1);
-    }
-    else
-    {
-        return (1);
-    }
+int i;
+for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+{
+	if (s1[i] != s2[i])
+{
+		return (s1[i] - s2[i]);
+}
+}
+if (s1[i] == s2[i])
+{
+	return (0);
+}
+else if (s1[i] == '\0')
+{
+	return (-1);
+}
+else
+{
+	return (1);
+}
 }
