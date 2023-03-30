cdb78733ecabb1b189d98e8814cda5e98c030c6e
ounadev <ounaissa19@gmail.com>
2023-03-30 02:51:09 -0700

    first

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * _strncat - Concatenates two strings
  * @dest: Destination string
@@ -11,18 +10,14 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-    int dest_len = 0, i = 0;
-
-    while (dest[dest_len] != '\0')
-        dest_len++;
-
-    while (i < n && src[i] != '\0')
-    {
-        dest[dest_len + i] = src[i];
-        i++;
-    }
-
-    dest[dest_len + i] = '\0';
-
-    return dest;
+int dest_len = 0, i = 0;
+while (dest[dest_len] != '\0')
+	dest_len++;
+while (i < n && src[i] != '\0')
+{
+	dest[dest_len + i] = src[i];
+	i++;
+}
+dest[dest_len + i] = '\0';
+return (dest);
 }
