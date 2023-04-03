481051b708bb198be007dcb8e2999c807b959568
ounadev <ounaissa19@gmail.com>
2023-04-03 08:49:42 -0700

    first commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _memset - Fills the first n bytes of the memory area pointed to by s with 
  * the constant byte b.
@@ -13,11 +12,9 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-
 	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
-
-	return (s);
+return (s);
 }
