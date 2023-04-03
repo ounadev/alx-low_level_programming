a446446ce782fb0bd6a8a691501faaa1132b8396
ounadev <ounaissa19@gmail.com>
2023-04-03 09:02:19 -0700

    first commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,17 +10,17 @@
  */
 char *_strchr(char *s, char c)
 {
-    while (*s != '\0')
-    {
-        if (*s == c)
-        {
-            return s;
-        }
-        s++;
-    }
-    if (*s == c)
-    {
-        return s;
-    }
-    return NULL;
+while (*s != '\0')
+{
+	if (*s == c)
+{
+		return s;
+}
+s++;
+}
+if (*s == c)
+{
+	return s;
+}
+return NULL;
 }
