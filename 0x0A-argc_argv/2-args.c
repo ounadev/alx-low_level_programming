a04e023f663f6b0fe7e9da5fa8096f2a53f420d0
ounadev <ounaissa19@gmail.com>
2023-04-10 06:40:22 -0700

    first

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-/*
-main - entry point function for printing all command line arguments
-@argc: number of arguments passed to the program
-@argv: array of strings containing all arguments passed to the program
-This function takes in the number of arguments passed to the program and an array
-of strings containing all the arguments. It then prints each argument on a separate
-line using a for loop.
-Return: 0 on successful execution
-*/
+/**
+ * main - entry point function for printing all command line arguments
+ * @argc: number of arguments passed to the program
+ * @argv: array of strings containing all arguments passed to the program
+ * Return: 0 on successful execution
+ */
 int main(int argc, char *argv[])
 {
 int i;
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main - Entry point
  * @argc: The number of command-line arguments
@@ -10,18 +9,15 @@
  */
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
-
-    if (argc != 3)
-    {
-        printf("Error\n");
-        return 1;
-    }
-
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[2]);
-    result = num1 * num2;
-
-    printf("%d\n", result);
-    return 0;
+int num1, num2, result;
+if (argc != 3)
+{
+	printf("Error\n");
+	return (1);
+}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+result = num1 * num2;
+printf("%d\n", result);
+return (0);
 }
