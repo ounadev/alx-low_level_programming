0a41aae3501bc4775e3a75c8f8a722b14c256f9f
ounadev <ounaissa19@gmail.com>
2023-04-10 06:52:59 -0700

    first

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * @argc: The number of arguments passed
+ * @argv: An array of pointers to the arguments
+ *
+ * Return: Always 0
+ */
 int main(int argc, char *argv[])
 {
   printf("%s\n", argv[0]);
-  return 0;
+
+  return (0);
 }
+
