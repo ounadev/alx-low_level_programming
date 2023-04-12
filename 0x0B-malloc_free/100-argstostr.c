049530735b993d6be0bbb935d17ffe4aa828e4f7
ounadev <ounaissa19@gmail.com>
2023-04-12 02:44:41 -0700

    first

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,42 +1,48 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * argstostr - Concatenates all the arguments of a program.
- * @ac: The number of arguments.
- * @av: An array of pointers to the arguments.
+ * argstostr - concatenates all the arguments of a program
+ * @ac: the number of arguments
+ * @av: an array of strings containing the arguments
  *
- * Return: If memory allocation fails, NULL.
- *         Otherwise, a pointer to the new string.
+ * Return: a pointer to the concatenated string, or NULL on failure
  */
 char *argstostr(int ac, char **av)
 {
-    char *str;
-    int i, j, len = 0, pos = 0;
+  char *str;
+  int i, j, len = 0, pos = 0;
 
-    if (ac == 0 || av == NULL)
-        return (NULL);
-for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j]; j++)
-            len++;
-        len++;
-    }
-str = malloc(len * sizeof(char));
-    if (str == NULL)
-        return (NULL);
- for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j]; j++)
-        {
-            str[pos] = av[i][j];
-            pos++;
-        }
-        str[pos] = '\n';
-        pos++;
-    }
+  if (ac == 0 || av == NULL)
+    return (NULL);
 
-    str[pos] = '\0';
-    return (str);
+  /* Calculate the total length of all strings */
+  for (i = 0; i < ac; i++)
+  {
+    for (j = 0; av[i][j] != '\0'; j++)
+      len++;
+    len++; /* Add one for the newline character */
+  }
+  len++; /* Add one for the null terminator */
+
+  /* Allocate memory for the concatenated string */
+  str = malloc(len * sizeof(char));
+  if (str == NULL)
+    return (NULL);
+
+  /* Copy the strings and add newline characters */
+  for (i = 0; i < ac; i++)
+  {
+    for (j = 0; av[i][j] != '\0'; j++)
+    {
+      str[pos] = av[i][j];
+      pos++;
+    }
+    str[pos] = '\n';
+    pos++;
+  }
+  str[pos] = '\0';
+
+  return (str);
 }
+
---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,85 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * count_words - Counts the number of words in a string
+ * @str: The string to count the words in
+ *
+ * Return: The number of words in the string
+ */
+int count_words(char *str)
+{
+    int i, count = 0;
+
+    for (i = 0; str[i] != '\0'; ++i)
+    {
+        if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+        {
+            ++count;
+        }
+    }
+
+    return (count);
+}
+
+/**
+ * strtow - Splits a string into words
+ * @str: The string to split
+ *
+ * Return: A pointer to an array of strings (words), or NULL on failure
+ */
+char **strtow(char *str)
+{
+    int i, j, k, len, words;
+    char **tab;
+
+    if (str == NULL || *str == '\0')
+    {
+        return (NULL);
+    }
+
+    words = count_words(str);
+
+    tab = malloc((words + 1) * sizeof(char *));
+    if (tab == NULL)
+    {
+        return (NULL);
+    }
+
+    for (i = 0, k = 0; i < words; ++i)
+    {
+        while (str[k] == ' ')
+        {
+            ++k;
+        }
+
+        len = 0;
+        while (str[k + len] != ' ' && str[k + len] != '\0')
+        {
+            ++len;
+        }
+
+        tab[i] = malloc((len + 1) * sizeof(char));
+        if (tab[i] == NULL)
+        {
+            for (j = 0; j < i; ++j)
+            {
+                free(tab[j]);
+            }
+            free(tab);
+            return (NULL);
+        }
+
+        for (j = 0; j < len; ++j)
+        {
+            tab[i][j] = str[k + j];
+        }
+        tab[i][j] = '\0';
+
+        k += len;
+    }
+
+    tab[i] = NULL;
+
+    return (tab);
+}
