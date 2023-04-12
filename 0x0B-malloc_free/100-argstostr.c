1cb6ab49d4569a4761dff533461ab681f3c97416
ounadev <ounaissa19@gmail.com>
2023-04-12 06:22:06 -0700

    first

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * argstostr - concatenates all the arguments of a program
  * @ac: the number of arguments
@@ -10,39 +9,31 @@
  */
 char *argstostr(int ac, char **av)
 {
-  char *str;
-  int i, j, len = 0, pos = 0;
-
-  if (ac == 0 || av == NULL)
-    return (NULL);
-
-  /* Calculate the total length of all strings */
-  for (i = 0; i < ac; i++)
-  {
-    for (j = 0; av[i][j] != '\0'; j++)
-      len++;
-    len++; /* Add one for the newline character */
-  }
-  len++; /* Add one for the null terminator */
-
-  /* Allocate memory for the concatenated string */
-  str = malloc(len * sizeof(char));
-  if (str == NULL)
-    return (NULL);
-
-  /* Copy the strings and add newline characters */
-  for (i = 0; i < ac; i++)
-  {
-    for (j = 0; av[i][j] != '\0'; j++)
-    {
-      str[pos] = av[i][j];
-      pos++;
-    }
-    str[pos] = '\n';
-    pos++;
-  }
-  str[pos] = '\0';
-
-  return (str);
+char *str;
+int i, j, len = 0, pos = 0;
+if (ac == 0 || av == NULL)
+	return (NULL);
+for (i = 0; i < ac; i++)
+{
+	for (j = 0; av[i][j] != '\0'; j++)
+		len++;
+	len++; /* Add one for the newline character */
+}
+len++; /* Add one for the null terminator */
+str = malloc(len * sizeof(char));
+if (str == NULL)
+	return (NULL);
+for (i = 0; i < ac; i++)
+{
+	for (j = 0; av[i][j] != '\0'; j++)
+{
+		str[pos] = av[i][j];
+		pos++;
+}
+	str[pos] = '\n';
+	pos++;
+}
+str[pos] = '\0';
+return (str);
 }
 
--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * count_words - Counts the number of words in a string
  * @str: The string to count the words in
@@ -9,17 +8,15 @@
  */
 int count_words(char *str)
 {
-    int i, count = 0;
-
-    for (i = 0; str[i] != '\0'; ++i)
-    {
-        if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
-        {
-            ++count;
-        }
-    }
-
-    return (count);
+int i, count = 0;
+for (i = 0; str[i] != '\0'; ++i)
+{
+	if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+{
+		++count;
+}
+}
+return (count);
 }
 
 /**
@@ -30,56 +27,47 @@ int count_words(char *str)
  */
 char **strtow(char *str)
 {
-    int i, j, k, len, words;
-    char **tab;
+int i, j, k, len, words;
+char **tab;
+if (str == NULL || *str == '\0')
+{
+	return (NULL);
+}
+words = count_words(str);
+tab = malloc((words + 1) * sizeof(char *));
+if (tab == NULL)
+{
+	return (NULL);
+}
+for (i = 0, k = 0; i < words; ++i)
+{
+	while (str[k] == ' ')
+{
+		++k;
+}
+	len = 0;
+	while (str[k + len] != ' ' && str[k + len] != '\0')
+{
+		++len;
+}
 
-    if (str == NULL || *str == '\0')
-    {
-        return (NULL);
-    }
-
-    words = count_words(str);
-
-    tab = malloc((words + 1) * sizeof(char *));
-    if (tab == NULL)
-    {
-        return (NULL);
-    }
-
-    for (i = 0, k = 0; i < words; ++i)
-    {
-        while (str[k] == ' ')
-        {
-            ++k;
-        }
-
-        len = 0;
-        while (str[k + len] != ' ' && str[k + len] != '\0')
-        {
-            ++len;
-        }
-
-        tab[i] = malloc((len + 1) * sizeof(char));
-        if (tab[i] == NULL)
-        {
-            for (j = 0; j < i; ++j)
-            {
-                free(tab[j]);
-            }
-            free(tab);
-            return (NULL);
-        }
-
-        for (j = 0; j < len; ++j)
-        {
-            tab[i][j] = str[k + j];
-        }
-        tab[i][j] = '\0';
-
-        k += len;
-    }
-
-    tab[i] = NULL;
-
-    return (tab);
+	tab[i] = malloc((len + 1) * sizeof(char))
+	if (tab[i] == NULL)
+{
+	for (j = 0; j < i; ++j)
+{
+		free(tab[j]);
+}
+	free(tab);
+	return (NULL);
+}
+	for (j = 0; j < len; ++j)
+{
+		tab[i][j] = str[k + j];
+}
+	tab[i][j] = '\0';
+	k += len;
+}
+tab[i] = NULL;
+return (tab);
 }
