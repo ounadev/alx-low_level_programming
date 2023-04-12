6aa9275fd2f401c0e5f234653d8531016d1ed6b3
ounadev <ounaissa19@gmail.com>
2023-04-12 08:51:48 -0700

    first

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+    _puts("Hello, world!");
+    int n = _abs(-10);
+    printf("Absolute value of -10 is: %d\n", n);
+    return (0);
+}
