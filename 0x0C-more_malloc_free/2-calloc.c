18c46e8ef9dcb1bbb49f56c19bd0f81943a375be
ounadev <ounaissa19@gmail.com>
2023-04-13 08:46:32 -0700

    first

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,22 +10,13 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    void *ptr;
-
-    /* check for zero arguments */
-    if (nmemb == 0 || size == 0)
-        return (NULL);
-
-    /* allocate memory using malloc */
-    ptr = malloc(nmemb * size);
-
-    /* check if malloc failed */
-    if (ptr == NULL)
-        return (NULL);
-
-    /* initialize memory to zero */
-    memset(ptr, 0, nmemb * size);
-
-    return (ptr);
+void *ptr;
+if (nmemb == 0 || size == 0)
+	return (NULL);
+ptr = malloc(nmemb * size);
+if (ptr == NULL)
+	return (NULL);
+memset(ptr, 0, nmemb * size);
+return (ptr);
 }
 
