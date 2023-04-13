7559839374e307f7df4cc12725132c0beca9723e
ounadev <ounaissa19@gmail.com>
2023-04-13 08:28:12 -0700

    first

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+void *malloc_checked(unsigned int b);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+
+#endif /* MAIN_H */
