aa4f6c4bb0172fcbf2d63fc6f4bf5393351056eb
ounadev <ounaissa19@gmail.com>
2023-04-17 08:35:37 -0700

    first

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ *
+ * Description: structure defining attributes of a dog
+ */
+
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+#endif /* DOG_H */
