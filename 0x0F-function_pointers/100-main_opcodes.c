38f523ae72abe49e4817613953d8f17fe74cefc9
ounadev <ounaissa19@gmail.com>
2023-04-19 08:06:48 -0700

    first

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main - prints the opcodes of its own main function
  * @argc: the number of arguments passed to the program
@@ -11,32 +10,25 @@
  */
 int main(int argc, char **argv)
 {
-    int i, num_bytes;
-
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return (1);
-    }
-
-    num_bytes = atoi(argv[1]);
-
-    if (num_bytes < 0)
-    {
-        printf("Error\n");
-        return (2);
-    }
-
-    /* print the opcodes */
-    for (i = 0; i < num_bytes; i++)
-    {
-        printf("%02x", *((unsigned char *)(main + i)));
-
-        if (i != num_bytes - 1)
-            printf(" ");
-        else
-            printf("\n");
-    }
-
-    return (0);
+int i, num_bytes;
+if (argc != 2)
+{
+	printf("Error\n");
+	return (1);
+}
+num_bytes = atoi(argv[1]);
+if (num_bytes < 0)
+{
+	printf("Error\n");
+	return (2);
+}
+for (i = 0; i < num_bytes; i++)
+{
+	printf("%02x", *((unsigned char *)(main + i)));
+	if (i != num_bytes - 1)
+		printf(" ");
+	else
+		printf("\n");
+}
+return (0);
 }
