a06ccf4cae2b71f416bf124f6f91b48ef375aee7
ounadev <ounaissa19@gmail.com>
2023-04-20 08:41:06 -0700

    first

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
-
 /**
  * print_numbers - Prints numbers, followed by a new line
  * @separator: The string to be printed between numbers
@@ -11,20 +10,15 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    va_list args;
-    unsigned int i;
-
-    va_start(args, n);
-
-    for (i = 0; i < n; i++)
-    {
-        printf("%d", va_arg(args, int));
-
-        if (i < n - 1 && separator)
-            printf("%s", separator);
-    }
-
-    printf("\n");
-
-    va_end(args);
+va_list args;
+unsigned int i;
+va_start(args, n);
+for (i = 0; i < n; i++)
+{
+	printf("%d", va_arg(args, int));
+	if (i < n - 1 && separator)
+		printf("%s", separator);
+}
+printf("\n");
+va_end(args);
 }
